4dca8ccb9bf385b46a8ef8e72b1dbc05b01ce010
Clay McGinnis <claymav@yahoo.com>
2017-01-10 00:14:20 -0600

    customized navigation pane

--- mobile/src/main/java/com/tripidevs/swoly/MainActivity.java
+++ mobile/src/main/java/com/tripidevs/swoly/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
