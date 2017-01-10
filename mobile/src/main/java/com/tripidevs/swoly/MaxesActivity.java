ae46cb9dc6a31fb2e528d4e2ca3c017c34911177
Innocent <25cent9@gmail.com>
2017-01-10 00:56:03 -0600

    Added maxes

---
+++ mobile/src/main/java/com/tripidevs/swoly/MaxesActivity.java
@@ -0,0 +1,16 @@
+package com.tripidevs.swoly;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by user on 1/10/2017.
+ */
+
+public class MaxesActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maxes);
+    }
+}
