15b254f284d44c561f6b82a5316fa093291a43ab
Clay McGinnis <claymav@yahoo.com>
2017-01-10 00:29:08 -0600

    changed id's on nav elements

--- mobile/src/main/java/com/tripidevs/swoly/MainActivity.java
+++ mobile/src/main/java/com/tripidevs/swoly/MainActivity.java
@@ -73,17 +73,19 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_schedule) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_log) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_maxes) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_signout) {
 
         }
 
