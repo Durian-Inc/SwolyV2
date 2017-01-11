eccfd0beb75834a618771140c795910ef5f201cd
griffin melson <griffinm962@gmail.com>
2017-01-11 01:08:59 -0600

    Began work on settings activity

--- mobile/src/main/java/com/tripidevs/swoly/MainActivity.java
+++ mobile/src/main/java/com/tripidevs/swoly/MainActivity.java
@@ -1,8 +1,10 @@
 package com.tripidevs.swoly;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -84,6 +86,15 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_profile) {
 
         } else if (id == R.id.nav_settings) {
+            try
+            {
+                Intent settings = new Intent(this,SettingsActivity.class);
+                startActivity(settings);
+            }
+            catch(Exception e)
+            {
+                Log.d("Opening settings error", e.toString());
+            }
 
         } else if (id == R.id.nav_signout) {
 
---
+++ mobile/src/main/java/com/tripidevs/swoly/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.tripidevs.swoly;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by griffin melson on 1/10/2017.
+ */
+
+public class SettingsActivity extends Activity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_settings);
+  }
+}
