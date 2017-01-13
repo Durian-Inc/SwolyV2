988d8dbc81c57d57c5f2fb108c91185179e2f132
Clay McGinnis <claymav@yahoo.com>
2017-01-13 00:42:39 -0600

    Changed the dynamics of SQL, added function for modifying and creating tables.

--- mobile/src/main/java/com/tripidevs/swoly/DBHandler.java
+++ mobile/src/main/java/com/tripidevs/swoly/DBHandler.java
@@ -13,17 +13,16 @@ public class DBHandler extends SQLiteOpenHelper {
 
     // All Static variables
     // Database Version
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     // Database Name
     private static final String DATABASE_NAME = "swolyItems";
 
     // Contacts table name
-    private static final String TABLE_ITEMS = "items";
+    private String TABLE_NAME = "items";
 
     // Contacts Table Columns names
     private static final String KEY_ID = "id";
-    private static final String KEY_ITEM = "item";
     private static final String KEY_VALUE = "value";
 
     // Constructor
@@ -31,20 +30,39 @@ public class DBHandler extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
+    public DBHandler(Context context, String table) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        this.TABLE_NAME = table;
+    }
+
+
     // Create Tables
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_ITEMS + "("
-                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_ITEM + " TEXT,"
-                + KEY_VALUE + " TEXT" + ")";
-        db.execSQL(CREATE_CONTACTS_TABLE);
+        String CREATE_ITEMS_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+                + KEY_ID + " INTEGER PRIMARY KEY,"
+                + KEY_VALUE + " INTEGER" + ")";
+        db.execSQL(CREATE_ITEMS_TABLE);
+    }
+
+    public void createTable() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String CREATE_ITEMS_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+                + KEY_ID + " INTEGER PRIMARY KEY,"
+                + KEY_VALUE + " INTEGER" + ")";
+        db.execSQL(CREATE_ITEMS_TABLE);
+    }
+
+    public void deleteTable() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
     }
 
     // Upgrade database
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         // Drop older table if existed
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_ITEMS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
 
         // Create tables again
         onCreate(db);
@@ -54,25 +72,23 @@ public class DBHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(KEY_ITEM, item.getItem()); // Item name
         values.put(KEY_VALUE, item.getValue()); // Item value
 
         // Inserting Row
-        db.insert(TABLE_ITEMS, null, values);
+        db.insert(TABLE_NAME, null, values);
         db.close(); // Closing database connection
     }
 
     public DatabaseItem getItem(int id) {
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.query(TABLE_ITEMS, new String[] { KEY_ID,
-                        KEY_ITEM, KEY_VALUE }, KEY_ID + "=?",
+        Cursor cursor = db.query(TABLE_NAME, new String[] { KEY_ID,
+                KEY_VALUE }, KEY_ID + "=?",
                 new String[] { String.valueOf(id) }, null, null, null, null);
         if (cursor != null)
             cursor.moveToFirst();
 
-        DatabaseItem item = new DatabaseItem(Integer.parseInt(cursor.getString(0)),
-                cursor.getString(1), Integer.parseInt(cursor.getString(2)));
+        DatabaseItem item = new DatabaseItem(Integer.parseInt(cursor.getString(0)), Integer.parseInt(cursor.getString(2)));
         // return item
         return item;
     }
@@ -80,7 +96,7 @@ public class DBHandler extends SQLiteOpenHelper {
     public List<DatabaseItem> getAllItems() {
         List<DatabaseItem> itemList = new ArrayList<DatabaseItem>();
         // Select All Query
-        String selectQuery = "SELECT  * FROM " + TABLE_ITEMS;
+        String selectQuery = "SELECT  * FROM " + TABLE_NAME;
 
         SQLiteDatabase db = this.getWritableDatabase();
         Cursor cursor = db.rawQuery(selectQuery, null);
@@ -90,8 +106,7 @@ public class DBHandler extends SQLiteOpenHelper {
             do {
                 DatabaseItem item = new DatabaseItem();
                 item.setID(Integer.parseInt(cursor.getString(0)));
-                item.setItem(cursor.getString(1));
-                item.setValue(Integer.parseInt(cursor.getString(2)));
+                item.setValue(Integer.parseInt(cursor.getString(1)));
                 // Adding item to list
                 itemList.add(item);
             } while (cursor.moveToNext());
@@ -102,7 +117,7 @@ public class DBHandler extends SQLiteOpenHelper {
     }
 
     public int getItemsCount() {
-        String countQuery = "SELECT  * FROM " + TABLE_ITEMS;
+        String countQuery = "SELECT  * FROM " + TABLE_NAME;
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(countQuery, null);
         cursor.close();
@@ -115,24 +130,23 @@ public class DBHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
-        values.put(KEY_ITEM, item.getItem());
         values.put(KEY_VALUE, item.getValue());
 
         // updating row
-        return db.update(TABLE_ITEMS, values, KEY_ID + " = ?",
+        return db.update(TABLE_NAME, values, KEY_ID + " = ?",
                 new String[] { String.valueOf(item.getID()) });
     }
 
     public void deleteItem(int item) {
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_ITEMS, KEY_ID + " = ?",
+        db.delete(TABLE_NAME, KEY_ID + " = ?",
                 new String[] { String.valueOf(item) });
         db.close();
     }
 
     public void deleteAllItems() {
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_ITEMS, "1", null);
+        db.delete(TABLE_NAME, "1", null);
         db.close();
     }
 }
