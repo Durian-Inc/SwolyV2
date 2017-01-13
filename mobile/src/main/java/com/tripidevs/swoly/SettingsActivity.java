f0fbf5052e7aec4c97a6a20af4cecc64538a0266
griffin melson <griffinm962@gmail.com>
2017-01-13 01:39:38 -0600

    Rewrote beginning of settings correctly

--- mobile/src/main/java/com/tripidevs/swoly/SettingsActivity.java
+++ mobile/src/main/java/com/tripidevs/swoly/SettingsActivity.java
@@ -2,15 +2,26 @@ package com.tripidevs.swoly;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
 /**
  * Created by griffin melson on 1/10/2017.
  */
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends PreferenceActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_settings);
+    getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+
+  }
+
+  public static class MyPreferenceFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(final Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      addPreferencesFromResource(R.xml.settings);
+    }
   }
 }
