d3e7384022abfc9310a44b747055dcd814a81a1a
griffin melson <griffinm962@gmail.com>
2017-01-14 03:01:38 -0600

    Formatting and more settings

--- mobile/src/main/java/com/tripidevs/swoly/MainActivity.java
+++ mobile/src/main/java/com/tripidevs/swoly/MainActivity.java
@@ -16,92 +16,89 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+  implements NavigationView.OnNavigationItemSelectedListener{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+    fab.setOnClickListener(new View.OnClickListener(){
+      @Override
+      public void onClick(View view){
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+          .setAction("Action", null).show();
+      }
+    });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
+    DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+      this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    drawer.setDrawerListener(toggle);
+    toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+    NavigationView navigationView = (NavigationView)findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
+  }
+
+  @Override
+  public void onBackPressed(){
+    DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+    if(drawer.isDrawerOpen(GravityCompat.START)){
+      drawer.closeDrawer(GravityCompat.START);
+    }else{
+      super.onBackPressed();
+    }
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item){
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if(id == R.id.action_settings){
+      return true;
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
+    return super.onOptionsItemSelected(item);
+  }
+
+  @SuppressWarnings("StatementWithEmptyBody")
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item){
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+
+    if(id == R.id.nav_home){
+      // Handle the camera action
+    }else if(id == R.id.nav_schedule){
+
+    }else if(id == R.id.nav_log){
+
+    }else if(id == R.id.nav_maxes){
+
+    }else if(id == R.id.nav_profile){
+
+    }else if(id == R.id.nav_settings){
+      try{
+        Intent settings = new Intent(this, SettingsActivity.class);
+        startActivity(settings);
+      }catch(Exception e){
+        Log.d("Opening settings error", e.toString());
+      }
+
+    }else if(id == R.id.nav_signout){
+
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_schedule) {
-
-        } else if (id == R.id.nav_log) {
-
-        } else if (id == R.id.nav_maxes) {
-
-        } else if (id == R.id.nav_profile) {
-
-        } else if (id == R.id.nav_settings) {
-            try
-            {
-                Intent settings = new Intent(this,SettingsActivity.class);
-                startActivity(settings);
-            }
-            catch(Exception e)
-            {
-                Log.d("Opening settings error", e.toString());
-            }
-
-        } else if (id == R.id.nav_signout) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
+    DrawerLayout drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+    drawer.closeDrawer(GravityCompat.START);
+    return true;
+  }
 }
--- mobile/src/main/java/com/tripidevs/swoly/SettingsActivity.java
+++ mobile/src/main/java/com/tripidevs/swoly/SettingsActivity.java
@@ -9,17 +9,17 @@ import android.preference.PreferenceFragment;
  * Created by griffin melson on 1/10/2017.
  */
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity{
   @Override
-  protected void onCreate(Bundle savedInstanceState) {
+  protected void onCreate(Bundle savedInstanceState){
     super.onCreate(savedInstanceState);
     getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
 
   }
 
-  public static class MyPreferenceFragment extends PreferenceFragment {
+  public static class MyPreferenceFragment extends PreferenceFragment{
     @Override
-    public void onCreate(final Bundle savedInstanceState) {
+    public void onCreate(final Bundle savedInstanceState){
       super.onCreate(savedInstanceState);
       addPreferencesFromResource(R.xml.settings);
     }
