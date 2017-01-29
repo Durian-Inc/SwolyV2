da9ff5b69a1d2dd46f7a972f00004a1c7975504b
Clay McGinnis <claymav@yahoo.com>
2017-01-28 19:39:03 -0600

    added chart to log page

--- mobile/src/main/java/com/tripidevs/swoly/log/LogFragment.java
+++ mobile/src/main/java/com/tripidevs/swoly/log/LogFragment.java
@@ -9,10 +9,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.utils.Utils;
 import com.tripidevs.swoly.DBHandler;
 import com.tripidevs.swoly.DatabaseItem;
 import com.tripidevs.swoly.R;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class LogFragment extends Fragment {
@@ -35,6 +43,40 @@ public class LogFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
+
+        DBHandler db = new DBHandler(getActivity(), "weight");
+
+        ArrayList<DatabaseItem> list = db.getAllItems();
+
+        DatabaseItem[] arr = list.toArray(new DatabaseItem[list.size()]);
+
+        List<Entry> entries = new ArrayList<Entry>();
+
+        for (DatabaseItem data : arr) {
+
+            // turn your data into Entry objects
+            Log.d("SQL: ", Integer.toString(data.getValue()));
+            entries.add(new Entry(data.getID(), data.getValue()));
+        }
+
+        Utils.init(getActivity());
+
+        LineChart chart = (LineChart) getView().findViewById(R.id.chart);
+
+        LineDataSet dataSet = new LineDataSet(entries, "Weight"); // add entries to dataset
+        dataSet.setColor(344489);
+        dataSet.setValueTextColor(299229);
+
+        Description e=new Description();
+        e.setText("");
+
+        chart.setDescription(e);
+        chart.setNoDataText("Log your Weight!");
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate();
+
 //        String tableName = "bench";
 //        DBHandler db = new DBHandler(getActivity(),tableName);
 
